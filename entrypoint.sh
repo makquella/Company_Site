0f361efe1d29264e5f356aa8843ff651653565fd
Artem Luhovyi <artem.lugovij2014@gmail.com>
2025-05-23 01:38:48 +0300

    Fix entrypoint.sh: LF endings + executable bit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env sh
-# Exit on error
 set -e
 
-python manage.py migrate --noinput
+python manage.py migrate
 
-python manage.py collectstatic --noinput
-
-exec "$@"
+exec gunicorn company_site.wsgi:application --bind 0.0.0.0:8000
